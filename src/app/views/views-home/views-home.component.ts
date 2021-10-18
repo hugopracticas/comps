90b55ced2ecb81de2f3186530ab5bf54d0e4d356
hugopracticas <hugoloyolamaya@gmail.com>
2021-10-18 15:47:58 -0500

    Se agrega el modulo de Views, con los componentes statistics y views-home

---
+++ src/app/views/views-home/views-home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-views-home',
+  templateUrl: './views-home.component.html',
+  styleUrls: ['./views-home.component.css']
+})
+export class ViewsHomeComponent implements OnInit {
+
+  stats = [
+    { value: 22, label: '# of Users' },
+    { value: 900, label: 'Revenue' },
+    { value: 50, label: 'Satisfaction Score'}
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
